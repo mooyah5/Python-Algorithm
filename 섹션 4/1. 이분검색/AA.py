8a68d72f59e18ba8112e1a25d178c41331b17de7
mooyah5 <qorgkssk123@gmail.com>
2023-04-07 03:59:13 +0900

    [섹션4]

---
+++ 섹션 4/1. 이분검색/AA.py
@@ -0,0 +1,39 @@
+# 이분검색
+
+# 임의의 N개의 숫자가 입력으로 주어진다.
+# N개의 수를 오름차 정렬한 후, 그 중 한 수인 M이 주어지면
+# 이분검색으로 몇 번째에 있는지 구해보렴
+# 1024개면, 1024-512-256... 이렇게 줄어들어서 log n 번이다 이말이야
+
+# 1) 재귀
+# def binary_search(M, arr, s, e):
+#     mid = (s+e) // 2
+#     if arr[mid] == M:
+#         return mid
+#     elif arr[mid] > M:
+#         e = mid-1
+#     else:
+#         s = mid+1
+#     return binary_search(M, arr, s, e)
+
+
+# N, M = map(int, input().split())
+# arr = list(map(int, input().split()))
+# arr.sort()
+# ans = 0
+# print(binary_search(M, arr, 0, N-1) + 1)  # arr, start, end
+
+# 2) while
+N, M = map(int, input().split())
+arr = list(map(int, input().split()))
+arr.sort()
+s, e = 0, N-1
+while (s <= e):
+    mid = (s+e) // 2
+    if arr[mid] == M:
+        print(mid+1)
+        break
+    elif arr[mid] > M:
+        e = mid - 1
+    else:
+        s = mid + 1
---
+++ 섹션 4/2. 랜선자르기/AA.py
@@ -0,0 +1,28 @@
+# 랜선자르기 (결정 알고리즘, 이분탐색)
+
+# 랜선의 개수 K
+# 잘린 랜선 길이는 최소 N개 이상이어야 한다.
+# K개의 랜선 길이가 주어지면, N개 이상으로 자를 수 있는 랜선 길이의 최대치를 출력하라.
+
+def count(mid):       # 해당 길이로 잘랐을 경우 만들어지는 랜선 개수
+    cnt = 0
+    for a in arr:
+        cnt += a//mid
+    return cnt
+
+
+K, N = map(int, input().split())
+arr = []
+for i in range(K):
+    arr.append(int(input()))
+
+s, e = 0, sum(arr)//N     # 이분탐색 위한 start, end 값
+res = 0
+while s <= e:
+    mid = (s+e)//2        # 중간 값 지정
+    if count(mid) >= N:   # 그 중간값이 N개 이상이면
+        res = mid             # res는 그 중간값이며
+        s = mid + 1           # start는 mid+1 부터로 올려준다.
+    else:                 # 그 중간값으로 N개 이상의 랜선을 만들지 못하면
+        e = mid - 1           # end 값을 mid-1 까지로 내려준다.
+print(res)
